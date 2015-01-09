d8c9fcf3a16ed069dfe980d3fb71ebd5f526fbbe
Daco Harkes <dc.harkes@gmail.com>
2015-01-09 14:27:01 +0100

    NaBL spec: first rudimentary name binding rules

---
+++ Booster2.5/trans/types.ts
@@ -0,0 +1,11 @@
+module trans/types
+// 
+// imports
+// 
+// type rules // references
+// 
+// 	Output(a) : ty
+// 	where definition of a : ty
+// 
+// 	PathStart(a, prime) : ty
+// 	where definition of a : ty
