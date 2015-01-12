c40a5827d0cabd45673ddda3e1c4389b483fc248
Daco Harkes <dc.harkes@gmail.com>
2015-01-12 13:55:28 +0100

    NaBL/TS spec: variables and paths desugar: specific  constructors for defs

--- Booster2.5/trans/types.ts
+++ Booster2.5/trans/types.ts
@@ -1,11 +1,41 @@
 module trans/types
-// 
-// imports
-// 
-// type rules // references
-// 
-// 	Output(a) : ty
-// 	where definition of a : ty
-// 
-// 	PathStart(a, prime) : ty
-// 	where definition of a : ty
+
+imports
+
+type rules // references
+
+	Input(a) : ty
+	where definition of a : ty
+
+	Output(a) : ty
+	where definition of a : ty
+
+	PathStart(a, prime) : ty
+	where definition of a : ty
+	
+	Path(path-start, path-component) : ty
+	where path-component : ty
+	
+	PathComponent(a, prime) : ty
+	where definition of a : ty
+
+	t@This() : ty
+	where definition of t : ty
+
+type rules // model refs
+
+	UniDirectional(s) : ty
+	where definition of s : ty
+
+	BiDirectional(class, attr): ty
+	where definition of class : ty
+	
+	Optional(s) : ty
+	where s : ty
+	
+	Set(s, mult) : ty
+	where s : ty
+
+type rules // primitives are their own type (so that a type-lookup can be done on the AST instead of using the AST itself)
+
+	ty@BasicType(x) : ty
