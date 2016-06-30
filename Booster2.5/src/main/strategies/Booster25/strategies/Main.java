4697bb376aab588e4dceadd5310a0c0afa7e7b9c
James Welch <james.welch@cs.ox.ac.uk>
2016-06-30 17:40:32 +0100

    Rename Booster25 to Booster2

--- Booster2.5/src/main/strategies/Booster25/strategies/Main.java
+++
@@ -1,9 +0,0 @@
-package Booster25.strategies;
-
-import org.strategoxt.lang.Context;
-
-public class Main {    
-    public static void init(Context context) {
-        
-    }
-}
---
+++ Booster2/src/main/strategies/Booster2/strategies/Main.java
@@ -0,0 +1,9 @@
+package Booster2.strategies;
+
+import org.strategoxt.lang.Context;
+
+public class Main {    
+    public static void init(Context context) {
+        
+    }
+}
